d6468ee760cd6630f06fe2b3c44abd9448f7ae72
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-08-21 14:46:18 +0545

    feat: add constants for available tokens in Monkey lang

--- GO/token.go
+++ GO/token.go
@@ -1,7 +1,35 @@
 package token
 
 type TokenType string
+
 type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+// available tokens
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
