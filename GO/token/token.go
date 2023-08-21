f0406c196f55cc8f19e888d830bde50d95a7cbb6
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-08-21 15:30:11 +0545

    feat: add function to identify keywords from identifiers

--- GO/token/token.go
+++ GO/token/token.go
@@ -33,3 +33,17 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent returns the TokenType for a given identifier
+// if exists else returns IDENT
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
